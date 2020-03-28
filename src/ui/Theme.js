de7686f92ff7851c0e7cf692218b0a1b328b9348
Meera <meera.bavadekar@gmail.com>
2020-03-27 21:36:03 -0700

    constant

---
+++ src/ui/Theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme} from '@material-ui/core/styles';
+
+
+export default createMuiTheme( {
+    palette: {
+        primary: {
+            main: '#3489ad'
+        },
+        secondary: {
+            main: '#8B008B'
+        }
+    }
+}
+
+)

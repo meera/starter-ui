05489dc299d1d32b483b552095c287391185dbc5
Meera <meera.bavadekar@gmail.com>
2020-04-15 00:09:01 -0700

    Added Login screen

---
+++ src/pages/ContactUs.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { makeStyles, useTheme} from "@material-ui/core/styles"
+import { Link} from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import background from '../assets/background.jpg';
+
+const useStyles = makeStyles( theme => ( {
+
+    backgrouund: {
+        backgroundImage: `url(${background} )`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "60em"
+    }
+}) );
+
+export default function AboutUs(props) {
+
+    const classes = useStyles();
+    const theme = useTheme();
+    
+    return (<Grid container direction="row"> 
+            <Grid item container direction="column">
+                <Grid item lg={3}> 
+                    <Typography variant="h2" style={{lineHeight: 1}}> Contact Us </Typography>
+                    <Typography variant="body1" style={{color: theme.palette.primary.main}}> We are waiting </Typography>
+
+                </Grid>
+                <Grid item container className={classes.backgrouund} lg={9}> 
+                </Grid>
+            </Grid>
+    </Grid>)
+}
+
